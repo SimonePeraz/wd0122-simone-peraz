a4322dcadc548e12f2bc9d90f5ba34a0445d0b6b
SimonePeraz <simoneperaz177@gmail.com>
2022-04-11 15:07:53 +0200

    proveJs

---
+++ m1/w3/d1/js/naming.js
@@ -0,0 +1,32 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto ';
+document.write(petPreferito)
+
+var PetPreferito = 'coniglio ';
+document.write(PetPreferito)
+
+
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
+
+var paragraph = 'jkgfjkbjnknnknfdnvnbdfmnbdnbnb nbnkdvb nm bvn fdmn mn mn mn cvn vn v n vn dn mnc mnv cn mncv cmn n'
+document.write(paragraph);
+
+var object = {
+    tipo: 'cane',
+    nome:'Silvano',
+    razza: 'segugio',
+    peso: 35,
+};
+console.log(object)
