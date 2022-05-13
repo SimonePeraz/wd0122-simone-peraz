39ef3196feb234e618af9a7f3b7d61969fb24410
SimonePeraz <simoneperaz177@gmail.com>
2022-05-13 14:19:02 +0200

    secondCommit

--- m2/w3/d5/js/script.js
+++ m2/w3/d5/js/script.js
@@ -1,6 +1,6 @@
 const JSON = '../json/users.json'
 
-let table = document.querySelector('#tabUsers')
+let tbody = document.querySelector('#tabUsers')
 
 fetch(JSON)
 .then(res => res.json())
@@ -8,22 +8,39 @@ fetch(JSON)
     console.log(res);
     for(let user of res){
         console.log(user.name);
-        let visualized = ["username","firstName","lastName","gender","profileURL","email"]
+        // let visualized = ["username","firstName","lastName","gender","profileURL","email"]
         let tr = document.createElement('tr')
+        tr.classList.add('row','p-3','pb-0','pt-0')
+
         let userTd = document.createElement('td')
         userTd.innerHTML = user.username
+        userTd.classList.add('col','p-4','bg-success','text-center','pb-0','pt-0')
+
         let firstNameTd = document.createElement('td')
         firstNameTd.innerHTML = user.firstName
+        firstNameTd.classList.add('col','p-4','bg-primary','text-center','pb-0','pt-0')
+
         let lastNameTd = document.createElement('td')
         lastNameTd.innerHTML = user.lastName
+        lastNameTd.classList.add('col','p-4','bg-success','text-center','pb-0','pt-0')
+
         let genderTd = document.createElement('td')
         genderTd.innerHTML = user.gender
+        genderTd.classList.add('col','p-4','bg-primary','text-center','pb-0','pt-0')
+
         let profileUrlTd = document.createElement('td')
-        profileUrlTd.innerHTML = user.profileUrl
+        let pic = document.createElement('img')
+        pic.setAttribute('src', user.profileURL )
+        profileUrlTd.append(pic) 
+        profileUrlTd.classList.add('col','p-4','bg-success','text-center','pb-0','pt-0')
+
         let emailTd = document.createElement('td')
         emailTd.innerHTML = user.email
+        emailTd.classList.add('col','p-4','bg-primary','text-center','pb-0','pt-0')
+
         tr.append(userTd,firstNameTd,lastNameTd,genderTd,profileUrlTd,emailTd)
-        table.append(tr)
+        tbody.append(tr)
     }
 })
 
+// `<img src=" ${utente.profileURL} ">`
